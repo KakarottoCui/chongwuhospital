d662a3fae3327e9407c9129abbcb37e37e8da9e1
cuifengbo <471363336@qq.com>
2025-03-05 21:38:17 +0800

    1

--- src/main/java/com/spring/dao/ChongwuzixunMapper.java
+++ src/main/java/com/spring/dao/ChongwuzixunMapper.java
@@ -1,11 +1,1 @@
-package com.spring.dao;
-
-import com.base.MapperBase;
-import com.spring.entity.Chongwuzixun;
-
-import org.springframework.stereotype.Repository;
-
-
-@Repository
-public interface ChongwuzixunMapper extends MapperBase<Chongwuzixun> {
-}
+获取源码加 dabocode
